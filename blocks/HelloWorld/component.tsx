8ea0f6860793657c023649e34834112b3f067764
mikecebul <mikecebul@gmail.com>
2025-08-28 10:43:54 -0400

    upgrade to v3, make sure dependencies are built

---
+++ blocks/HelloWorld/component.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import type { HelloWorldBlock as HelloWorldBlockType } from "@/payload-types";
+
+export function HelloWorldBloack({
+  title,
+  content,
+  buttonText,
+}: HelloWorldBlockType) {
+  return (
+    <div className="p-6 border rounded-lg">
+      <h2 className="text-xl font-semibold mb-4">{title}</h2>
+      <p className="text-muted-foreground mb-4">{content}</p>
+      <Button>{buttonText}</Button>
+    </div>
+  );
+}
